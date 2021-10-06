a7e25563c7f8814c0e064fa942d1f203660855c9
Fernando Franco <fernandofrancoprofissinal@gmail.com>
2021-10-06 19:48:18 -0300

    sass installed and fix packages versions

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
   mode:'production',
   entry: path.resolve(__dirname,'src/js/index.js'),
@@ -6,5 +8,20 @@ module.exports = {
     path: __dirname,
     publicPath: __dirname,
     filename: 'script.js'
-  }
+  },
+  plugins: [new MiniCssExtractPlugin({
+    filename: 'styles.css'
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
 }
